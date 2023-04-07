80c11797733e70ee8e9cbd575f9d47622674deea
anjaliruth <ruthanjali0211@gmail.com>
2023-04-07 11:14:04 +0800

    Test passed, with answers inside

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 /* 
 👉 Write your kata here!
 */
+// Unique Letters: Write a function that takes in a string and returns True if all the letters in the string are unique and False otherwise. For example, "hello" should return False because it contains the letter "l" twice, while "world" should return True.
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function uniqueLetters(string) {
+    let seenLetters = new Set()
+
+    for (let i = 0; i < string.length; i++)
+    {
+        let letter = string[i].toLowerCase()
+
+        if (seenLetters.has(letter)) {
+            return false
+        }
+        else {
+            seenLetters.add(letter)
+        }
+    }
+    return true
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,30 @@
 //👉 Write your tests below here:
+import {test, expect} from "@jest/globals"
+
+import{uniqueLetters} from "./main.js"
+
+
+test("Does input hello return false", function () {
+    let actual = uniqueLetters("hello")
+expect(actual).toBe(false)
+})
+
+test("Does input grand return false", function () {
+    let actual = uniqueLetters("grand")
+expect(actual).toBe(true)
+})
+
+test("Does same letter in different cases return false", function () {
+    let actual = uniqueLetters("WLKDwlkd")
+expect(actual).toBe(false)
+})
+
+test("Does mix of number and letters return true", function () {
+    let actual = uniqueLetters("Wo0d23456")
+expect(actual).toBe(true)
+})
+
+test("Does repeated lettrs in different order return false", function () {
+    let actual = uniqueLetters("loleh")
+expect(actual).toBe(false)
+})
